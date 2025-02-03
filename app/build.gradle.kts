7f4467bb2817d7bcecd212611f20a7c7db0179dc
juanarton <juanarton@protonmail.com>
2025-02-03 23:36:04 +0700

    fix: Fix bug, update dependencies, move to libs.versions.toml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,7 +44,7 @@ android {
             )
         }
         debug {
-            isMinifyEnabled = true
+            isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
